7dd5935921e89d0a66d7cdbe1d56562060dc5718
Antoni Lương Phạm Anh Quân <Antoni.LuongPham@gmail.com>
2022-07-14 10:09:38 +0700

    HW1 and HW2 Programs

---
+++ STEAM-for-VietNam/2022-06-CS102-PyGame-Course/Lesson-01/Crazy-Robot/Lesson_2_HW1.py
@@ -0,0 +1,89 @@
+from typing import List
+import pygame
+from common import (
+    ROBOT_SPRITE, TO_MO_SPRITE, PLAYER_SPRITE,
+    WHITE, BACKGROUND_SPRITE, FPS, screen, clock
+)
+from entities import (
+    Player, Robot,
+    GameItem, ItemType, NPC, GameState, GameStateType,
+)
+from utils import overlap
+
+
+# Game States:
+player = Player(350, 200, TO_MO_SPRITE)
+
+list_robot: List[Robot] = [
+    Robot(500, 500, 1, 1, ROBOT_SPRITE),
+    Robot(50, 50, -2, 2, ROBOT_SPRITE),
+    Robot(50, 10, 3, 5, ROBOT_SPRITE)
+]
+
+list_item: List[GameItem] = [
+    GameItem(600, 500, ItemType.DIAMOND_BLUE),
+    GameItem(800, 500, ItemType.DIAMOND_RED),
+    GameItem(1000, 400, ItemType.DIAMOND_RED),
+    GameItem(100, 500, ItemType.DIAMOND_BLUE),
+    GameItem(880, 700, ItemType.DIAMOND_RED),
+    GameItem(500, 400, ItemType.DIAMOND_RED),
+]
+
+to_mo: NPC = NPC(1000, 50, PLAYER_SPRITE)
+
+# Bắt đầu game
+game_state: GameState = GameState(score=0)
+
+running: bool = True
+while running:
+    # Người chơi có tắt màn hình game chưa
+    if pygame.event.peek(pygame.QUIT):
+        running = False
+        break
+
+    # ----------------------------------------
+    if game_state.state == GameStateType.RUNNING:
+        player.update()
+
+        for robot in list_robot:
+            robot.update()
+
+        for item in list_item:
+            if not item.hidden:
+                if overlap(player.x, player.y, player.image, item.x, item.y, item.image):
+                    item.set_hidden()
+                    # Increase Score
+                    new_score: int = game_state.score + 1
+                    game_state.update_score(new_score)
+
+        for robot in list_robot:
+            if overlap(player.x, player.y, player.image, robot.x, robot.y, robot.image):
+                print("YOU LOST!!")
+                game_state.update_state(GameStateType.LOST)
+
+        if overlap(player.x, player.y, player.image, to_mo.x, to_mo.y, to_mo.image):
+            print("YOU WON!!")
+            game_state.update_state(GameStateType.WON)
+
+    # ----------------------------------------
+    # Vẽ các vật phẩm game
+    screen.fill(WHITE)
+    screen.blit(BACKGROUND_SPRITE, (0, 0))
+
+    player.render(screen)
+
+    for robot in list_robot:
+        robot.render(screen)
+
+    for item in list_item:
+        item.render(screen)
+
+    to_mo.render(screen)
+
+    game_state.render(screen)
+
+    pygame.display.flip()
+    clock.tick(FPS)
+
+# Ket thuc game
+pygame.quit()
