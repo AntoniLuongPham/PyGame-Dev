ec0289cf8f1ba63c192433887b173cd2610c7667
Antoni Lương Phạm Anh Quân <Antoni.LuongPham@gmail.com>
2022-07-23 23:45:52 -0700

    move old Python programs to Python-Programming repo

--- STEAM-for-VietNam/2022-06-CS102-PyGame-Course/Antoni's projects/Antoni.py
+++
@@ -1,6 +0,0 @@
-from typing import List
-names_of_the_luong_family: List[str] = ["Antoni", "Ariana", "Mom", "Dad"]
-
-for name in names_of_the_luong_family:
-    print(name)
-    print(str(f'My name is {name}.'))
--- STEAM-for-VietNam/2022-06-CS102-PyGame-Course/Antoni's projects/example_dict.py
+++
@@ -1,6 +0,0 @@
-from typing import Dict
-
-minecraft: Dict[str, str] = {
-    "mine": "survive",
-    "craft": "make"
-}
