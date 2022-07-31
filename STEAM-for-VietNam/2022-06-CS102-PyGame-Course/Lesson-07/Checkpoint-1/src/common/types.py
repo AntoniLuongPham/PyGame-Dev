f242884b8d89271ecbf7ff94602055b6d08f23f2
Antoni Lương Phạm Anh Quân <Antoni.LuongPham@gmail.com>
2022-07-30 20:39:28 -0700

    add Lesson-7 Checkpoint-1

---
+++ STEAM-for-VietNam/2022-06-CS102-PyGame-Course/Lesson-07/Checkpoint-1/src/common/types.py
@@ -0,0 +1,25 @@
+import enum
+
+
+class ActionType(enum.Enum):
+    IDLE = "idle"
+    JUMP = "jump"
+    MOVE = "move"
+    CRAWL = "crawl"
+
+
+class EntityType(enum.Enum):
+    # The images of these types will be scaled down to GameConfig.tile_size right after loading.
+    EMPTY = 0
+    GROUND = 1
+    HEART = 2
+    CANDY = 3
+    SHADOW = 9
+
+    PLAYER = 41
+    NPC_CO_NGA = 42
+
+
+OBSTACLES_TYPES = (EntityType.GROUND,)
+COLLECTABLE_TYPES = (EntityType.HEART, EntityType.CANDY)
+FRIENDLY_NPC_TYPES = (EntityType.NPC_CO_NGA,)
